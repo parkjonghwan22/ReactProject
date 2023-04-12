9beb6be3d0fd62264407a495cbe9a4b55d7a6737
parkjonghwan22 <gch02933@naver.com>
2023-04-13 00:35:09 +0900

    feat: 오류수정

--- front/src/common/header/Logo/logo.jsx
+++ front/src/common/header/Logo/logo.jsx
@@ -1,13 +1,13 @@
-import { useLocation } from 'react-router-dom';
-import {MainLogoStyled} from "./styled"
-import blackLogo from "../../images/blackLogo.png"
-import whiteLogo from "../../images/Logo.png"
+import { useLocation } from "react-router-dom";
+import { MainLogoStyled } from "./styled";
+import blackLogo from "../../images/blackLogo.png";
+import whiteLogo from "../../images/Logo.png";
 
-export const MainLogo = ({Logocheck}) =>{
-    const logoImg = Logocheck === "blackLogo" ? blackLogo : whiteLogo
-    return (
-        <MainLogoStyled Img={logoImg}>
-            <div></div>
-        </MainLogoStyled>
-    )
-}
+export const MainLogo = ({ Logocheck }) => {
+  const logoImg = Logocheck === "blackLogo" ? blackLogo : whiteLogo;
+  return (
+    <MainLogoStyled Img={logoImg}>
+      <div></div>
+    </MainLogoStyled>
+  );
+};
