9686857df8a42bd8703fc55b3fd54121d7060f27
parkjonghwan22 <gch02933@naver.com>
2023-04-13 10:19:10 +0900

    feat: Abc 버튼 css 수정

--- front/src/pages/music/styled/musicform.styled.jsx
+++ front/src/pages/music/styled/musicform.styled.jsx
@@ -2,4 +2,8 @@ import styled from "styled-components";
 
 export const Musicform = styled.div`
   margin: 50px 0 auto;
+
+  & > div:nth-child(2) > div:nth-child(3) {
+    background-color: green;
+  }
 `;
