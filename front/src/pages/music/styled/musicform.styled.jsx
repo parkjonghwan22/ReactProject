41fbf3489b6f612f08a25c087b041b160d682aee
parkjonghwan22 <gch02933@naver.com>
2023-04-13 10:30:46 +0900

    feat: Abc 버튼 CSS 수정

--- front/src/pages/music/styled/musicform.styled.jsx
+++ front/src/pages/music/styled/musicform.styled.jsx
@@ -3,7 +3,12 @@ import styled from "styled-components";
 export const Musicform = styled.div`
   margin: 50px 0 auto;
 
-  & > div:nth-child(2) > div:nth-child(3) {
-    background-color: green;
+  & > div:nth-child(2) > button {
+    border: 1px solid #fff;
+    color: #fff;
+    margin-top: 5px;
+    margin-right: 5px;
+    padding: 5px;
+    box-sizing: border-box;
   }
 `;
